2d99fb472cdf6bb69d58919536ff0ff852991a8e
Rubozs <Ruben2412000@gmail.com>
2018-08-23 19:39:15 +0200

    First run android

--- frontend/WordShare/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ frontend/WordShare/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,22 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "com.cordova.plugins.cookiemaster.cookieMaster",
-    "file": "plugins/com.cordova.plugins.cookiemaster/www/cookieMaster.js",
-    "pluginId": "com.cordova.plugins.cookiemaster",
-    "clobbers": [
-      "cookieMaster"
-    ]
-  },
-  {
-    "id": "cordova-plugin-cookieemperor.cookieEmperor",
-    "file": "plugins/cordova-plugin-cookieemperor/www/cookieEmperor.js",
-    "pluginId": "cordova-plugin-cookieemperor",
-    "clobbers": [
-      "cookieEmperor"
-    ]
-  },
-  {
     "id": "cordova-plugin-statusbar.statusbar",
     "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
     "pluginId": "cordova-plugin-statusbar",
@@ -28,8 +12,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "com.cordova.plugins.cookiemaster": "1.0.0",
-  "cordova-plugin-cookieemperor": "1.0.0",
   "cordova-plugin-statusbar": "2.4.1",
   "cordova-plugin-whitelist": "1.3.3"
 };
--- frontend/WordShare/plugins/cordova-plugin-cookieemperor/src/ios/CDVCookieEmperor.h
+++
@@ -1,9 +0,0 @@
-#import <Cordova/CDVPlugin.h>
-
-@interface CDVCookieEmperor : CDVPlugin
-
-- (void)getCookieValue:(CDVInvokedUrlCommand*)command;
-- (void)setCookieValue:(CDVInvokedUrlCommand*)command;
-- (void)clearCookies:(CDVInvokedUrlCommand*)command;
-
-@end

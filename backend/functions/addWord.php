085665951642f284a3d4f43faea9ef802e918ab6
Lenard Strahringer <spamlenards@gmail.com>
2018-08-23 16:35:48 +0200

    addWord Änderungen

--- backend/functions/addWord.php
+++ backend/functions/addWord.php
@@ -1,40 +1,42 @@
 <?php
 
-#fügt Wörter in die Datenbank hinzu
+#Verifikationsdatei - ordnet Wort von w_verification nach word oder sortiert Wort aus
 
 #Variablen initialisieren
-$newword = $_POST['word'];
-//aus Mangel an besseren Ideen:
+$wordtoverificate = $_GET['wordid'];
+$bewertung = $_GET['validation']; #kann Werte 1 und 0 annehmen
+$verificator_id = $_GET['verificator'];
 
-$schonDa = True;
 #Datenbankverbindung
 require_once("../inc/db.php");
 
-#Prüfe, ob Wort bereits existiert
+#Abfrage: Bewertung positiv oder negativ
+
+if ($bewertung == 1){
+#Neues Wort zu word hinzufügen
 $db->query("SET NAMES 'utf8'");
-$sql = "SELECT * FROM words WHERE word='{$newword}'";
+$sql = "SELECT * FROM w_verification WHERE id='{$wordtoverificate}'";
 $erg = $db->query($sql);
-    if (!$erg)
-    {
-      $schonDa = False;
-    }
-  $row = $erg->fetch_assoc(); 
-
-
-
-#Daten aus der Datenbank holen
-$db->query("SET NAMES 'utf8'");
-$sql = "SELECT * FROM words WHERE word='{$newword}'";
-$erg = $db->query($sql);
-    if (!$erg)
-    {
-      $schonDa = False;
-    }
+    if (!$erg){
+    die ('Etwas stimmte mit der Abfrage nicht: '.$db->error);
+  }
   $row = $erg->fetch_assoc();
 
-#Validation
-if($pw_hash == row["pwhash"]) print("1");
-else print("0");
+#Neuer Datensatz in Tabelle word
+$db->query("SET NAMES 'utf8'");
+$sql = "INSERT INTO word (word, translation, w_code, t_code, creator_id, verificator_id)
+VALUES ('{$row["word"]}', '{$row["translation"]}', '{$row["w_code"]}','{$row["t_code"]}','{$row["creator_id"]}','{$verificator_id}')";
+$erg = $db->query($sql);
+if (!$erg){
+die ('Etwas stimmte mit der Abfrage nicht: '.$db->error);
+}}
 
+#Datensatz aus w_verification löschen
+$sql ="DELETE FROM w_verification
+WHERE id = ".$row["id"];
+$erg = $db->query($sql);
+if (!$erg){
+die ('Etwas stimmte mit der Abfrage nicht: '.$db->error);
 
+}
  ?>

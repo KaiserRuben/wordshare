b89e13542212d4bd7cf6659938c6e63de52db232
Rubozs <Ruben2412000@gmail.com>
2018-08-23 14:16:59 +0200

    LoginScript finished

---
+++ backend/functions/login.php
@@ -0,0 +1,25 @@
+<?php
+
+#LoginDatei für unsere Anwenung, gibt 1 oder 0 zurück.
+
+#Variablen initialisieren
+$username = $_POST['name'];
+$pw_hash = password_hash($_POST['pw']);
+#Datenbankverbindung
+require_once("../inc/db.php");
+
+#Daten aus der Datenbank holen
+$db->query("SET NAMES 'utf8'");
+$sql = "SELECT COUNT(*) FROM user WHERE username='{$username}'";
+$erg = $db->query($sql);
+    if (!$erg){
+    die ('Etwas stimmte mit der Abfrage nicht: '.$db->error);
+  }
+  $row = $erg->fetch_assoc();
+
+#Validation
+if($row['COUNT(*)']==1) print("1");
+else print("0");
+
+
+ ?>

664babff2cf0cd3be200492eae8a18cbf725a97f
Lenard Strahringer <spamlenards@gmail.com>
2018-08-24 19:55:35 +0200

    display existing words

---
+++ backend/functions/exwords.php
@@ -0,0 +1,32 @@
+<?php
+#Variablen initialisieren
+$input = $_GET['input'];
+
+#Datenbankverbindung
+require_once("../inc/db.php");
+
+#Datenbanken nach existierenden Worten durchsuchen
+$db->query("SET NAMES 'utf8'");
+$sql = "SELECT word FROM words WHERE *;"
+$erg = $db->query($sql);
+$words = $erg->fetch_assoc();
+
+// Worte zählen
+$wordnumber = int count ( mixed $words [, int $mode = COUNT_NORMAL ] );
+
+//länge input
+$strlenght = int strlen ( string $input );
+
+//Worte mit selbem Anfang ermitteln
+$counter = int 0;
+$result = "";
+while (!$counter == $wordnumber){
+$w_beginn = string substr ( string $words[$counter] , int 0 [, int $strlenght] );
+if ( $w_beginn == $words[$counter]){
+  $result = $result + ", " $words[$counter];
+  $counter = $counter + 1;
+}
+}
+print ($result);
+
+?>

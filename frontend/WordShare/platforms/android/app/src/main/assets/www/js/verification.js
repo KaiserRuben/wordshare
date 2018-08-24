4aca849bb493e8c682356bcd1b80de2387677a9e
Rubozs <Ruben2412000@gmail.com>
2018-08-24 19:38:37 +0200

    Bugfixing und EMail funnction added

--- frontend/WordShare/platforms/android/app/src/main/assets/www/js/verification.js
+++ frontend/WordShare/platforms/android/app/src/main/assets/www/js/verification.js
@@ -2,7 +2,7 @@
 //Username wird als Cookie gespeiert
 setCookie("username", username);
 
-function registrieren(){
+function validate(){
   var username = document.getElementById("uname").value;
   var val = document.getElementById("").value;
   var word = document.getElementById("").value;
@@ -30,4 +30,58 @@ function registrieren(){
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     xmlHttp.send( null );
     return xmlHttp.responseText;
+  }
+
+
+
+
+
+
+
+
+
+  datenSammeln();
+
+  function datenSammeln(){
+
+    if(checkCookie("username")&&checkCookie("password")){
+
+      var username = getCookie("username");
+      var pw = getCookie("password");
+
+    anmelden(username, password);
+
+    }
+    else{
+
+      var username = document.getElementById("name").value;
+      var pw = document.getElementById("pw").value;
+
+      if(pw!=""&&username!=""){
+        anmelden(username, password);
+      }
+
+    }
+  }
+
+
+  function anmelden(username, password){
+
+    //Username wird als Cookie gespeiert
+    setCookie("username", username);
+
+    var url = "http://wordshare.kaiseritea.de/server/functions/login.php?name=" + username + "&&pw=" + pw;
+    var success = httpGet(url);
+    console.log(success);
+
+    if(document.getElementById("check").checked == true){
+      setCookie("password", pw);
+    }
+
+    if(success == "1"){
+      location="https://www.bing.de";
+    }
+    else{
+      alert("Wrong Password or Username!");
+    }
   }

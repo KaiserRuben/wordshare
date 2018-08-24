4fa0afb58f8bfadd85c470dbf1668e6c76853be7
Rubozs <Ruben2412000@gmail.com>
2018-08-24 22:23:50 +0200

    Mail corrections

--- backend/functions/register.php
+++ backend/functions/register.php
@@ -21,17 +21,15 @@ $erg = $db->query($sql);
 
   $empfaenger = $mail;
   $betreff = 'Herzlich Willkommen';
-  $nachricht = 'Hallo,<br /> wir begrüßen dich herzlich und danken dir, dass du dich bei WordShare registriert hast. <br />
+  $nachricht = 'Hallo '.$name.',<br /><br />Wir begrüßen dich herzlich und danken dir, dass du dich bei WordShare registriert hast. <br />
   Wir freuen uns sehr darüber, dass du WordShare mit deinen Ideen bereichern wirst. <br />
   Außerdem bitten wir dich, zu bedenken, dass die App von Nutzern erstellt wurde und diese seriös zu behandeln ist. <br />
   Wir wünschen dir viel Spaß beim Hinzufügen neuer Vokabeln und beim entspannten Erlernen einer neuen Sprache. <br />
   Bis gleich! <br />
   Dein WordShare-Team' ;
-  $header = array(
-     'From' => 'team@wordshare.com',
-     'X-Mailer' => 'PHP/' . phpversion()
-  );
-  $header[] = 'Content-type: text/html; charset=iso-8859-1';
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+  $headers .= 'From: <Team@WordShare.com>' . "\r\n";
 
   mail($empfaenger, $betreff, $nachricht, $header);
   #Erfolg?

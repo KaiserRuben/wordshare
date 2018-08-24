f5d5db03e72da16d802d36189a5bbb5ccc7ad8ff
Lenard Strahringer <spamlenards@gmail.com>
2018-08-24 11:12:16 +0200

    sendwords edit

--- frontend/WordShare/www/js/sendword.js
+++ frontend/WordShare/www/js/sendword.js
@@ -1,12 +1,12 @@
 function sendword() {
-#get Eingaben des Benutzers
+//get Eingaben des Benutzers
   var word  = document.getElementById('word');
   var transword  = document.getElementById('translation');
-  var w_code = "en";
-  var t_code = "de";
-  var creator_id = 1;
+  var w_code = "EN";
+  var t_code = "DE";
+  var user = getCookie(cname);
 
-#sende Daten an addWord
+//sende Daten an addWord
     xhttp.open("GET", "../../../../backend/functions/addWord.php", true);
     xhttp.send();
 

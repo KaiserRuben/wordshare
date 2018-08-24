043dda8305144217c2cf47fd07dc547041c28f87
Rubozs <Ruben2412000@gmail.com>
2018-08-24 11:05:58 +0200

    Wenn man vergisst die Datei vorher zu speichern... Siehe letzter Commit

--- backend/functions/getWord.php
+++ backend/functions/getWord.php
@@ -1,39 +1,49 @@
 <?php
-#fügt Wörter in die Datenbank hinzu
-#gibt 1 bei Erfolg (Wort noch nicht eingetragen), sonst 0 zurück
+/*
+Dieses geniale Script ist geschrieben um zu dem Nutzer passende Wörter aus der Datenbak zu  holen.
+
+Was reinkommt:  Der Nutername als _GET (kommt aus daily-word.html)
+Was rausgeht:   Ein JSON mit Wort, Übersetzung, jeweils den Language-Codes und dem Timestamp.
+
+Ein gutes Lied zum Programmieren: https://www.youtube.com/watch?v=knfrxj0T5NY
+Nevermind ich finde Trap Nation ist generell nice, gerade zum Programmieren (chillig und so)
+
+By the Way:
+Windows ist wie ein U-Boot,sobalt man ein Fenster öffnet,fangen die Probleme an.
+*/
 
 #Variablen initialisieren
-$newword = $_POST['word'];
-$translation = $_POST['translation'];
-$w_code = $_GET['w_code'];
-$t_code= $_GET['t_code'];
-$creator_id = $_GET['creator_id'];
+$username = $_GET['username'];
+
 
 #Datenbankverbindung
 require_once("../inc/db.php");
 
-#Prüfe, ob Wort bereits existiert
+#Nutzerdaten holen
 $db->query("SET NAMES 'utf8'");
-$sql = "SELECT * FROM words WHERE word='{$newword}'";
+$sql = "SELECT * FROM user WHERE username='{$username}'";
 $erg = $db->query($sql);
-    if (!$erg){
-      $sql = "SELECT * FROM w_verification WHERE word='{$newword}'";
-      $erg = $db->query($sql);
-          if (!$erg){
-            $sql = "INSERT INTO w_verification (word, translation, w_code, t_code, creator_id)
-            VALUES ('{$newword}', '{$translation}', '{$w_code}','{$t_code}','{$creator_id}')";
-            $erg = $db->query($sql);
-            if ($erg){
-              $success = true;
-            }
-          }
-    }
-    else{
-      die ('Dieses Wort ist bereits in der Datenbank '.$db->error);
-    }
+  if (!$erg){
+    die ('Nutzer ist wahrscheinlich nicht in der Datenbank: '.$db->error);
+  }
+$row = $erg->fetch_assoc();
 
+#Userpräferenzen festlegen um passende Wörter zu finden
+$mt = $row["mt"];
+$nl = $row["nl"];
+$bekannteWorterArray = explode(";", $row["nl"];)
 
-#Erfolg?
-if($success == true) print("1");
-else print("0");
+#und jeeetzt bereiten wir den sql dynamisch praktisch gut vor
+$db->query("SET NAMES 'utf8'");
+$sql = "SELECT * FROM words WHERE w_code='{$nl}' AND t_code='{$ml}' ORDER BY id DESC LIMIT 10";
+#Das führen wir dann mal aus...
+$erg = $db->query($sql);
+  if (!$erg){
+    die ('Nutzer ist wahrscheinlich nicht in der Datenbank: '.$db->error);
+  }
+$row = $erg->fetch_assoc();
+
+#Und da wir es gerne als JSON hätten convertieren wir es noch und geben es dann aus.
+$resultJSON = json_encode($row);
+print($resultJSON);
 ?>

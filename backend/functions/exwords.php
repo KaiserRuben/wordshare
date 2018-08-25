988d9a9d6bf09494a7e6f7c7e1b4f6041e2f5f03
Lenard Strahringer <spamlenards@gmail.com>
2018-08-25 11:37:41 +0200

    exwords.php

--- backend/functions/exwords.php
+++ backend/functions/exwords.php
@@ -1,34 +1,36 @@
 <?php
 #Variablen initialisieren
 $input = $_GET['input'];
-
 #Datenbankverbindung
 require_once("../inc/db.php");
 
 #Datenbanken nach existierenden Worten durchsuchen
 $db->query("SET NAMES 'utf8'");
-$sql = "SELECT word FROM words WHERE *";
+$sql = "SELECT word FROM words";
 $erg = $db->query($sql);
-$words = $erg->fetch_assoc();
+$words = $erg->fetch_array();
 
-// Worte zählen
-$wordnumber = count ( $words );
+# Worte zählen
+$wordnumber = mysql_num_rows($words);
+print($wordnumber);
 
-//Länge input
+#$wordnumber = count ( $words );
+
+#Länge input
 $strlenght = strlen ( $input );
 
-//Worte mit selbem Anfang ermitteln
+#Worte mit selbem Anfang ermitteln
 $counter = 0;
 $result = " ";
-while (!$counter == $wordnumber){
+while (!($counter == $wordnumber)){
 $w_beginn = substr ($words[$counter] , 0 , $strlenght);
+print ($words[$counter]);
 
-// Worte aneinander fügen
+# Worte aneinander fügen
 if ( $w_beginn == $words[$counter]){
   $result = $result + ", "+ $words[$counter];
-  $counter = $counter + 1;
 }
+  $counter++;
 }
 print ($result);
-
 ?>

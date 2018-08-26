37bfe2933e6b60403dfc6744376a312ad3cb292c
Lenard Strahringer <spamlenards@gmail.com>
2018-08-26 09:46:39 +0200

    .

--- frontend/WordShare/www/js/game.js
+++ frontend/WordShare/www/js/game.js
@@ -1,12 +1,6 @@
 function fillGame(){
   //Eingabe vorhanden?
 //Ausgabe der vorhandenen Worte
-
-var words = httpGet("http://wordshare.kaiseritea.de/server/functions/fillgame.php");
-print("hallo");
-console.log(words);
-//document.getElementById("txtHint").innerHTML = exwords;
-
 var url = "http://wordshare.kaiseritea.de/server/functions/getWord.php?username=" + getCookie("username") + "&&type=game";
 var response = httpGet(url);
 var wordsJSON = JSON.parse(response);
@@ -14,7 +8,12 @@ console.log(wordsJSON);
 document.getElementById('word').innerHTML = wordsJSON[0].word;
 document.getElementById('translation').innerHTML = wordsJSON[0].translation;
 
+
+document.getElementById("box1").innerHTML = wordsJSON[0].word;
+
+
 }
+
 
 function httpGet(theUrl){
   var xmlHttp = new XMLHttpRequest();

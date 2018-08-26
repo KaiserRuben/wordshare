97caa6f4f6245983a7df434f3eddfe4cffbc7735
Lenard Strahringer <spamlenards@gmail.com>
2018-08-26 10:36:57 +0200

    game

--- frontend/WordShare/www/js/game.js
+++ frontend/WordShare/www/js/game.js
@@ -9,7 +9,18 @@ console.log(wordsJSON);
 //document.getElementById('translation').innerHTML = wordsJSON[0].translation;
 
 
-document.getElementById("box1").innerHTML = wordsJSON[0].word;
+document.getElementById("box1").innerHTML = wordsJSON[2].word;
+document.getElementById("box2").innerHTML = wordsJSON[1].translation;
+document.getElementById("box3").innerHTML = wordsJSON[5].word;
+document.getElementById("box4").innerHTML = wordsJSON[0].translation;
+document.getElementById("box5").innerHTML = wordsJSON[3].word;
+document.getElementById("box6").innerHTML = wordsJSON[4].translation;
+document.getElementById("box7").innerHTML = wordsJSON[1].word;
+document.getElementById("box8").innerHTML = wordsJSON[0].word;
+document.getElementById("box9").innerHTML = wordsJSON[5].translation;
+document.getElementById("box10").innerHTML = wordsJSON[2].translation;
+document.getElementById("box11").innerHTML = wordsJSON[3].translation;
+document.getElementById("box12").innerHTML = wordsJSON[4].word;
 
 
 }
